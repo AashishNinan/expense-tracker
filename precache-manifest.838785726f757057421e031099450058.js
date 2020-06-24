e78563ab9b4c30de773f0e0cd5d0c89c32365b7f
AashishNinan <53154814+AashishNinan@users.noreply.github.com>
2020-06-24 20:41:32 +0530

    Updates

---
+++ precache-manifest.838785726f757057421e031099450058.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d943574b94fc620d62573bcf8cdb60d5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9c37bfcefbcf36d519a5",
+    "url": "/static/css/main.bccd0344.chunk.css"
+  },
+  {
+    "revision": "51ff54a4a79138cefe59",
+    "url": "/static/js/2.41db281f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.41db281f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c37bfcefbcf36d519a5",
+    "url": "/static/js/main.41b182b8.chunk.js"
+  },
+  {
+    "revision": "5b66c22e2d8482e69f0e",
+    "url": "/static/js/runtime-main.2fca4b70.js"
+  }
+]);
